81ee4f893466520df9f4497f1cdb0bb2e374775b
Aniece Thompson <aniecet@yahoo.com>
2019-10-14 03:11:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+  attr_reader :balance, :name, :status
 
+  def initialize(name)
+    @name=name
+    @balance = 1000
+    @status= "open"
+  end 
+  
+  def deposit
+    @balance += deposit
+    @balance
+  end
 end
